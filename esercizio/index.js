fa35930ef05f5cae0f2475d0c94259ccad7737f6
DanieleTesti <danieletesti14@gmail.com>
2022-12-15 16:50:13 +0100

    update

---
+++ esercizio/index.js
@@ -0,0 +1,56 @@
+//\u00D7 SERVE PER ELIMINARE
+//   Crea un pulsante e lo aggiunge a ciascun elemento dell'elenco
+let listaNodo = document.getElementsByTagName("li");
+for (let i = 0; i < listaNodo.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  listaNodo[i].appendChild(span);
+}
+
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+let list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.tagName === "LI") {
+      event.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+// Creazione nuovo elemento alla to do list
+function newElement() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("Input").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("Input").value = "";
+
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
